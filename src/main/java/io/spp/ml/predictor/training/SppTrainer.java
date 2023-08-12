35b858289ded3e68635f787c6bf93806c8d737db
Akshay.Jain <2021MT93641@wilp.bits-pilani.ac.in>
2023-08-12 11:25:30 +0530

    spp ml

--- src/main/java/io/spp/ml/predictor/training/SppTrainer.java
+++ src/main/java/io/spp/ml/predictor/training/SppTrainer.java
@@ -1,6 +1,11 @@
 package io.spp.ml.predictor.training;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import org.apache.spark.sql.Column;
 import org.apache.spark.sql.Dataset;
@@ -8,11 +13,20 @@ import org.apache.spark.sql.Row;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import io.spp.ml.predictor.dao.SppMLTrainingDao;
+import lombok.extern.log4j.Log4j2;
 
+@Log4j2
 public class SppTrainer
 {
     @Autowired
     private SppMLTrainingDao sppMLTrainingDao;
+
+    private ExecutorService executorService;
+
+    public SppTrainer()
+    {
+        executorService = Executors.newWorkStealingPool();
+    }
     
     public void train()
     {
@@ -22,19 +36,44 @@ public class SppTrainer
         Dataset<Row> securityReturnDf = sppMLTrainingDao.loadSecurityReturns(exchangeCodesList);
         Dataset<Row> securityAnalyticsDf = sppMLTrainingDao.loadSecurityAnalytics(exchangeCodesList);
         Dataset<Row> securityTrainingPScoreDf = sppMLTrainingDao.loadSecurityTrainingPScore(exchangeCodesList);
-        
-        Dataset<Row> securityDataAll = securityReturnDf.withColumnRenamed("returns", "securityReturns")
+
+        Dataset<Row> securityDataAll = securityReturnDf.select(new Column("exchange"), new Column("exchangeCode"), new Column("isin"), new Column("date"), new Column("returns.90D.return").as("securityReturns90D"))
                                                                               .join(securityAnalyticsDf, new String[] {"exchange", "exchangeCode", "isin", "date"})
-                                                                              .join(indexReturnDf.withColumnRenamed("returns", "indexReturns"), new String[] {"exchange", "date"})
-                                                                              .join(securityTrainingPScoreDf, new String[] {"exchange", "exchangeCode", "isin", "date"})
+                                                                              .join(securityTrainingPScoreDf.select(new Column("exchange"), new Column("index"), new Column("exchangeCode"), new Column("isin"), new Column("date"), new Column("trainingPScore.90D.pScore").as("trainingPScore90D")), new String[] {"exchange", "exchangeCode", "isin", "date"})
+                                                                              .join(indexReturnDf.select(new Column("exchange"), new Column("index"), new Column("date"), new Column("returns.90D.return").as("indexReturns90D")), new String[] {"exchange", "index", "date"})
                                                                               .orderBy("date");
 
 
+        List<CompletableFuture<Dataset<Row>>> futures = new ArrayList<>();
+
         exchangeCodesList.forEach(r->{
+            CompletableFuture<Dataset<Row>> future = CompletableFuture.supplyAsync(new SppTrainingTask(r.getString(0), securityDataAll), executorService);
+            futures.add(future);
+        });
+        
+        CompletableFuture<Void> allDone = CompletableFuture.allOf(futures.toArray(new CompletableFuture[] {}));
+        while(!allDone.isDone())
+        {
+            try
+            {
+                Thread.sleep(1000);
+            }
+            catch (InterruptedException e)
+            {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+        
+        futures.forEach(f->{
             
-            String exchangeCode = r.getString(0);
-            Dataset<Row> securityDataForExchangeCode = securityDataAll.where(new Column("exchangeCode").equalTo(exchangeCode));
-            securityDataForExchangeCode.printSchema();
+            try 
+            {
+                f.get();
+            } catch(ExecutionException | InterruptedException e)
+            {
+                log.error(e);
+            }
             
         });
     }
--- src/main/java/io/spp/ml/predictor/training/SppTrainingTask.java
+++ src/main/java/io/spp/ml/predictor/training/SppTrainingTask.java
@@ -1,99 +1,105 @@
-//package io.spp.ml.predictor.training;
-//
-//import javax.annotation.Nullable;
-//
-//import org.apache.spark.ml.feature.VectorAssembler;
-//import org.apache.spark.ml.linalg.Vectors;
-//import org.apache.spark.ml.regression.LinearRegression;
-//import org.apache.spark.ml.regression.LinearRegressionModel;
-//import org.apache.spark.ml.regression.LinearRegressionSummary;
-//import org.apache.spark.ml.regression.LinearRegressionTrainingSummary;
-//import org.apache.spark.sql.Dataset;
-//import org.apache.spark.sql.Row;
-//import org.springframework.beans.factory.annotation.Autowired;
-//
-//import io.spp.ml.predictor.dao.SppMLTrainingDao;
-//import lombok.RequiredArgsConstructor;
-//
-//@RequiredArgsConstructor
-//public class SppTrainingTask implements Runnable
-//{
-//    @Nullable
-//    private final String exchangeCode;
-//    
-//    @Autowired
-//    private SppMLTrainingDao sppMLTrainingDao;
-//
-//    @Override
-//    public void run()
-//    {
-//        Dataset<Row> indexReturnDf = sppMLTrainingDao.loadIndexReturns();
-//        Dataset<Row> securityReturnDf = sppMLTrainingDao.loadSecurityReturns(exchangeCode);
-//        Dataset<Row> securityAnalyticsDf = sppMLTrainingDao.loadSecurityAnalytics(exchangeCode);
-//        Dataset<Row> securityTrainingPScoreDf = sppMLTrainingDao.loadSecurityTrainingPScore(exchangeCode);
-//
-//        
-//        Dataset<Row> indexReturnDfForTraining = indexReturnDf.select("exchange", "index", "date", "returns.90D.return").withColumnRenamed("return", "indexReturn");
-//        Dataset<Row> securityReturnDfForTraining = securityReturnDf.select("exchange", "exchangeCode", "isin", "date", "returns.90D.return").withColumnRenamed("return", "securityReturn");
-//        Dataset<Row> securityAnalyticsDfForTraining = securityAnalyticsDf.select("exchange", "exchangeCode", "isin", "date",
-//                                                                                                              "analytics.obv","analytics.ema","analytics.ad","analytics.wma","analytics.stoch.SlowK","analytics.stoch.SlowD","analytics.trima","analytics.rsi"
-//                                                                                                              ,"analytics.tema","analytics.mama.MAMA","analytics.mama.FAMA","analytics.bbands.Real Upper Band","analytics.bbands.Real Middle Band"
-//                                                                                                              ,"analytics.bbands.Real Lower Band","analytics.adx","analytics.sma","analytics.aroon.Aroon Down","analytics.aroon.Aroon Up","analytics.kama"
-//                                                                                                              ,"analytics.cci","analytics.macdext.MACD","analytics.macdext.MACD_Signal","analytics.macdext.MACD_Hist","analytics.t3");
-//        
-//        Dataset<Row> securityTrainingPScoreDfForTraining =  securityTrainingPScoreDf.select("exchange", "exchangeCode", "isin", "date", "trainingPScore.90D.pScore");
-//        
-//        Dataset<Row> trainingData = securityReturnDfForTraining
-//                                                    .join(securityAnalyticsDfForTraining, new String[] {"exchange", "exchangeCode", "isin", "date"})
-//                                                    .join(indexReturnDfForTraining, new String[] {"exchange", "date"})
-//                                                    .join(securityTrainingPScoreDfForTraining, new String[] {"exchange", "exchangeCode", "isin", "date"})
-//                                                    .orderBy("date")                                                   
-//                                                    .select("pScore", "indexReturn", "securityReturn",
-//                                                            "obv","ema","ad","wma","SlowK","SlowD","trima","rsi"
-//                                                            ,"tema","MAMA","FAMA","Real Upper Band","Real Middle Band"
-//                                                            ,"Real Lower Band","adx","sma","Aroon Down","Aroon Up","kama"
-//                                                            ,"cci","MACD","MACD_Signal","MACD_Hist","t3");
-//        
-//        VectorAssembler trainingDataAssembler = new VectorAssembler()
-//                                                                        .setInputCols(new String[]{"indexReturn", "securityReturn",
-//                                                                                "obv","ema","ad","wma","SlowK","SlowD","trima","rsi"
-//                                                                                ,"tema","MAMA","FAMA","Real Upper Band","Real Middle Band"
-//                                                                                ,"Real Lower Band","adx","sma","Aroon Down","Aroon Up","kama"
-//                                                                                ,"cci","MACD","MACD_Signal","MACD_Hist","t3"})
-//                                                                        .setOutputCol("features");
-//        
-//        Dataset<Row> trainingDataDf = trainingDataAssembler.transform(trainingData).select("pScore", "features").withColumnRenamed("pScore", "label");
-//        
-//        Dataset<Row> trainingSetDf = trainingDataDf.limit((int)(trainingDataDf.count()/2));
-//        Dataset<Row> validationSetDf = trainingDataDf.except(trainingSetDf);
-//        
-//        LinearRegression lr = new LinearRegression().setMaxIter(10000).setRegParam(0.3).setElasticNetParam(0.8);
-//        
-//        // Fit the model.
-//        LinearRegressionModel lrModel = lr.fit(trainingSetDf);
-//        
-//        System.out.println("Coefficients: " + lrModel.coefficients() + " Intercept: " + lrModel.intercept());
-//        
-//        // Summarize the model over the training set and print out some metrics.
-//        LinearRegressionTrainingSummary trainingSummary = lrModel.summary();
-//        System.out.println("numIterations: " + trainingSummary.totalIterations());
-//        System.out.println("objectiveHistory: " + Vectors.dense(trainingSummary.objectiveHistory()));
-//        trainingSummary.residuals().show();
-//        System.out.println("RMSE: " + trainingSummary.rootMeanSquaredError());
-//        System.out.println("r2: " + trainingSummary.r2());
-//        
-//        LinearRegressionSummary validationSummary = lrModel.evaluate(validationSetDf);
-//        System.out.println("meanAbsoluteError: " + validationSummary.meanAbsoluteError());
-//        validationSummary.predictions().show(false);
-//        validationSummary.residuals().show(false);
-//        System.out.println("RMSE: " + validationSummary.rootMeanSquaredError());
-//        System.out.println("r2: " + validationSummary.r2());
-//        
-//    }
-//    
-////    private SppMLTrainingDao getSppMLTrainingDao()
-////    {
-////        return SpringContextHolder.getBean(SppMLTrainingDao.class);
-////    }
-//
-//}
+package io.spp.ml.predictor.training;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.concurrent.Callable;
+import java.util.function.Supplier;
+
+import org.apache.spark.ml.feature.VectorAssembler;
+import org.apache.spark.ml.linalg.Vectors;
+import org.apache.spark.ml.regression.LinearRegression;
+import org.apache.spark.ml.regression.LinearRegressionModel;
+import org.apache.spark.ml.regression.LinearRegressionSummary;
+import org.apache.spark.ml.regression.LinearRegressionTrainingSummary;
+import org.apache.spark.sql.Column;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+
+import io.fop.context.impl.ApplicationContextHolder;
+import io.spp.ml.predictor.SppGlobalContext;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class SppTrainingTask implements Callable<Dataset<Row>>, Supplier<Dataset<Row>>
+{
+    private final String exchangeCode;
+    
+    private final Dataset<Row> securityDataAll;
+    
+    @Override
+    public Dataset<Row> call()
+    {
+        return buildModel();
+    }
+    
+    @Override
+    public Dataset<Row> get()
+    {
+        return buildModel();
+    }
+
+    private Dataset<Row> buildModel()
+    {
+        Dataset<Row> securityDataForExchangeCode = securityDataAll.where(new Column("exchangeCode").equalTo(exchangeCode));
+        
+        Dataset<Row> trainingData = securityDataForExchangeCode.select("date", "exchange", "index", "exchangeCode", "isin", "securityReturns90D", "indexReturns90D"
+                                                                                                          , "analytics.obv","analytics.ema","analytics.ad","analytics.wma","analytics.stoch.SlowK","analytics.stoch.SlowD","analytics.trima","analytics.rsi"
+                                                                                                          , "analytics.tema","analytics.mama.MAMA","analytics.mama.FAMA","analytics.bbands.Real Upper Band","analytics.bbands.Real Middle Band"
+                                                                                                          , "analytics.bbands.Real Lower Band","analytics.adx","analytics.sma","analytics.aroon.Aroon Down","analytics.aroon.Aroon Up","analytics.kama"
+                                                                                                          , "analytics.cci","analytics.macdext.MACD","analytics.macdext.MACD_Signal","analytics.macdext.MACD_Hist","analytics.t3"
+                                                                                                          , "trainingPScore90D"
+                                                                                                        );
+        
+        SppGlobalContext sppGlobalContext = (SppGlobalContext)ApplicationContextHolder.getGlobalContext(SppGlobalContext.class.getSimpleName());
+        String trainingStartDateStr = sppGlobalContext.fetch(SppGlobalContext.trainingStartDateKey);
+        String trainingEndDateStr = sppGlobalContext.fetch(SppGlobalContext.trainingEndDateKey);
+        LocalDate trainingStartDate = LocalDate.parse(trainingStartDateStr);
+        LocalDate trainingEndDate = LocalDate.parse(trainingEndDateStr);
+        long days = ChronoUnit.DAYS.between(trainingStartDate, trainingEndDate.plusDays(1));
+        long trainingSetDays = (long)(days * 0.75);
+        LocalDate trainingSetEndDate = trainingStartDate.plusDays(trainingSetDays);
+        String trainingSetEndDateStr = trainingSetEndDate.format(DateTimeFormatter.ISO_LOCAL_DATE);
+        
+        
+        
+        VectorAssembler trainingDataAssembler = new VectorAssembler()
+                                                                        .setInputCols(new String[]{"securityReturns90D", "indexReturns90D"
+                                                                                , "obv","ema","ad","wma","SlowK","SlowD","trima","rsi"
+                                                                                , "tema","MAMA","FAMA","Real Upper Band","Real Middle Band"
+                                                                                , "Real Lower Band","adx","sma","Aroon Down","Aroon Up","kama"
+                                                                                , "cci","MACD","MACD_Signal","MACD_Hist","t3"})
+                                                                        .setOutputCol("features");
+        
+        Dataset<Row> trainingDataVectorized = trainingDataAssembler.transform(trainingData).select("date", "trainingPScore90D", "features").withColumnRenamed("trainingPScore90D", "label");
+        
+        Dataset<Row> trainingSetData = trainingDataVectorized.where(new Column("date").leq(trainingSetEndDateStr)).select("label", "features");
+        Dataset<Row> validationSetData = trainingDataVectorized.where(new Column("date").gt(trainingSetEndDateStr)).select("label", "features");
+
+        LinearRegression lr = new LinearRegression().setMaxIter(10000).setRegParam(0.3).setElasticNetParam(0.8);
+
+        // Fit the model.
+        LinearRegressionModel lrModel = lr.fit(trainingSetData);
+        
+        System.out.println("Coefficients: " + lrModel.coefficients() + " Intercept: " + lrModel.intercept());
+
+        // Summarize the model over the training set and print out some metrics.
+        LinearRegressionTrainingSummary trainingSummary = lrModel.summary();
+        System.out.println("numIterations: " + trainingSummary.totalIterations());
+        System.out.println("objectiveHistory: " + Vectors.dense(trainingSummary.objectiveHistory()));
+        trainingSummary.residuals().show();
+        System.out.println("RMSE: " + trainingSummary.rootMeanSquaredError());
+        System.out.println("r2: " + trainingSummary.r2());
+
+        LinearRegressionSummary validationSummary = lrModel.evaluate(validationSetData);
+        System.out.println("meanAbsoluteError: " + validationSummary.meanAbsoluteError());
+        validationSummary.predictions().show(false);
+        validationSummary.residuals().show(false);
+        System.out.println("RMSE: " + validationSummary.rootMeanSquaredError());
+        System.out.println("r2: " + validationSummary.r2());
+        
+        return null;
+    }
+
+}
